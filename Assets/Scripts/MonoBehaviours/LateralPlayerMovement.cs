dddc9ad338c146badceba31fa503f12de3e18f36
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-02-13 02:24:27 -0800

    added player movement

---
+++ Assets/Scripts/MonoBehaviours/LateralPlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class LateralPlayerMovement : MonoBehaviour
+{
+    [SerializeField] Transform mainCamera;
+    [SerializeField] CharacterController cc;
+    [SerializeField] float speed = 100f;
+    [SerializeField] float smoothTime = 0.01f;
+
+    float curVelocity = 0f;
+    float horizontal, vertical;
+    float camYRot;
+
+    void Start()
+    {
+        camYRot = mainCamera.rotation.eulerAngles.y;
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        HandleMove();
+    }
+
+    void HandleMove()
+    {
+        Vector3 inputVec = new Vector3(horizontal, 0f, vertical).normalized;
+        if (inputVec.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(inputVec.x, inputVec.z) * Mathf.Rad2Deg + mainCamera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref curVelocity, smoothTime);
+
+            Vector3 moveDir = Quaternion.Euler(new Vector3(0f, targetAngle, 0f)) * Vector3.forward;
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            cc.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/MonoBehaviours/VerticalPlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class VerticalPlayerMovement : MonoBehaviour
+{
+    [SerializeField] CharacterController cc;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayerMask;
+    [SerializeField] float groundCheckRadius;
+    [SerializeField] float jumpDist;
+
+    float yVel = 0f;
+    float gravity = Physics.gravity.y;
+
+    void Update()
+    {
+        yVel += gravity * Time.deltaTime;
+        cc.Move(new Vector3(0f, yVel * Time.deltaTime, 0f));
+
+        HandleJump();
+
+        if (isGrounded() && yVel <= 0f)
+        {
+            yVel = 0f;
+        }
+
+    }
+
+    bool isGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayerMask);
+    }
+
+    void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        {
+            yVel = Mathf.Sqrt(2 * -gravity * jumpDist);
+        }
+    }
+
+}
